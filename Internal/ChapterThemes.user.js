521fc3ca07f623209a9c18fc27487f99c58c1493
Sollace <mwchris@mweb.co.za>
2014-10-18 12:47:07 +0200

    * Fixed some things + Added more flexability + Custom styles are now applied when the page loads

--- Internal/ChapterThemes.user.js
+++ Internal/ChapterThemes.user.js
@@ -2,7 +2,7 @@
 // @name        Fimfiction Chapter Themes API
 // @author      Sollace
 // @namespace   fimfiction-sollace
-// @version     1
+// @version     1.0.1
 // @include     http://www.fimfiction.net/*
 // @include     https://www.fimfiction.net/*
 // @grant       none
@@ -19,44 +19,49 @@ var ponyThemes = (function() {
       document.body.appendChild(style);
     }
   }
+  function createStyleSet(styles) {
+    var result = '';
+    for (var i in styles) result += i + ': ' + styles[i].replace(/ \!important/g, '') + ' !important;';
+    return result;
+  }
   function ponyTheme(name, style) {
     this.Name = name.replace(/_/g, ' ');
     name = name.replace(/ /g, '_');
     this.option = '<option value="' + name + '">' + this.Name + '</option>';
     this.css = '';
-    if (style['story']) this.css += '\
-.content_format_' + name + ' a.story_name {\
-  color: ' + style.story.color + ' !important;}';
-    if (style['content']) this.css += '\
-.content_format_' + name + ' {\
-  background: ' + style.content.background + ' !important;\
-  color: ' + style.content.color + ' !important;}';
-  if (style['p']) this.css += '\
-.content_format_' + name + ' p {\
-  padding: 10px;\
-  background: ' + style.p.background + ' !important;\
-  color: ' + style.p.color + ' !important;\
-  margin-top: 0px;\
-  padding-top: 10px;\
-  padding-bottom: 10px;}';
-  if (style['pnth']) this.css += '\
-.content_format_babs p:nth-child(' + style.pnth.n + ') {\
-  background-color: ' + style.pnth.background + ' !important;\
-  color: ' + style.pnth.color + ' !important;}';
+    if (style['story']) this.css += '.content_format_' + name + ' a.story_name {' + createStyleSet(style.story) + '}';
+    if (style['content']) this.css += '.content_format_' + name + ' {' + createStyleSet(style.content) + '}';
+    if (style['p']) this.css += '.content_format_' + name + ' p {' + createStyleSet(style.p) + '}';
+    if (style['pnth']) this.css += '.content_' + name + ' p:nth-child(' + style.pnth.n + ') {' + createStyleSet(style.pnth) + '}';
+  }
+  if ($('#format_colours').attr('apiInit') != '1') {
+    $('#format_colours').attr('apiInit', '1');
+    $(document).ready(function() {
+      $('#format_colours').on('change', function() {
+        unsafeWindow.LocalStorageSet( "format_colours_2", $("#format_colours").val());
+      });
+    });
   }
   return {
     apply: function() {
       var sheet = '';
       for (var i in data) {
-        sheet += data[i].css;
-        $('optgroup[label="Ponies"]').append(data[i].option);
+        for (var j in data[i]) {
+          sheet += data[i][j].css;
+          if (!$('optgroup[label="' + i + '"]').length) {
+            $('optgroup[label="Ponies"]').after('<optgroup label="' + i + '" />');
+          }
+          $('optgroup[label="' + i + '"]').append(data[i][j].option);
+        }
       }
       addGlobalStyle(sheet);
-    }, add: function(name, style) {
-      data[name] = new ponyTheme(name, style);
-      return name;
-    }, remove: function(name) {
-      data[name] = null;
+      $('#format_colours').val(unsafeWindow.LocalStorageGet('format_colours_2', unsafeWindow.LocalStorageGet('format_colours', 'bow')));      unsafeWindow.UpdateColours();
+    }, add: function(category, name, style) {
+      if (!data[category]) data[category] = {};
+      data[category][name] = new ponyTheme(name, style);
+      return {one: category, two: name};
+    }, remove: function(key) {
+      data[key.one][key.two] = null;
     }
   }
 })();
