058c9359d1f9e6f9264165d8188bedf39a893923
Sollace <mwchris@mweb.co.za>
2016-06-23 17:21:38 +0200

    * (Interactive Ponies) Bump version

--- Interactive Ponies/Interactive_Ponies.user.js
+++ Interactive Ponies/Interactive_Ponies.user.js
@@ -7,7 +7,7 @@
 // @include     http://justsitback.deviantart*
 // @include     http://www.fimfiction.net/*
 // @include     https://www.fimfiction.net/*
-// @version     1.7.2
+// @version     1.8
 // @grant       none
 // ==/UserScript==
 
